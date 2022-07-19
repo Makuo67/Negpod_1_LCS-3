ce31f1df9b9ef2dcfccb8a60ec284831d7658d0d
Oluwabunmi1 <debanjobunmi@gmail.com>
2022-07-19 22:29:44 +0300

    LCS-2

---
+++ main.py
@@ -0,0 +1,28 @@
+month_of = input("Enter Month: ")
+month = month_of.lower()
+
+if month == "january":
+    print("The month of January has 31 days.")
+elif month == "february":
+    print("The mont of February has 28 0r 29 days.")
+elif month == "march":
+    print("The month of march has 31 days.")
+elif month == "april":
+    print("The month of April has 30 days.")
+elif month == "may":
+    print("The month of May has 31 days.")
+elif month == "june":
+    print("The month of june has 30 days.")
+elif month == "july":
+    print("The month of july has 31 days.")
+elif month == "august":
+    print("The month of august has 31 days.")
+elif month == "september":
+    print("The month of september has 30 days.")
+elif month == "october":
+    print("The month of october has 31 days.")
+elif month == "November":
+    print("The month of November has 30 days.")
+elif month == "December":
+    print("The month of December has 31 days.")
+
